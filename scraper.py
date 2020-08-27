c1a95bfb9d80cd6e53d571aaf60489fd2349d381
hicham20201441 <70323150+hicham20201441@users.noreply.github.com>
2020-08-27 14:00:41 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
-
+# -*- coding: utf-8 -*-
 import scraperwiki
 import lxml.html
 #

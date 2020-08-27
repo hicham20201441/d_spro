4eafa49c23aba79da80d2d26cdfb6af950c04375
hicham20201441 <70323150+hicham20201441@users.noreply.github.com>
2020-08-27 12:34:35 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ html = scraperwiki.scrape("https://dollarsprout.com")
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 for e in root.cssselect("a"):
-  print(e["href"])
+  print(e.get("href"))
   #scraperwiki.sqlite.save(unique_keys=[e.get("href")], data={"link": e.get("href")})
 
 #root.cssselect("div[class='blog-col']")

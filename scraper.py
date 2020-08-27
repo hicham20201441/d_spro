0acba9b8bf76e627f612dab2650caceb837ae8f8
hicham20201441 <70323150+hicham20201441@users.noreply.github.com>
2020-08-27 12:48:34 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,13 +4,35 @@
 import scraperwiki
 import lxml.html
 #
+url="https://dollarsprout.com"
 # # Read in a page
-html = scraperwiki.scrape("https://dollarsprout.com")
+html = scraperwiki.scrape(url)
 #
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-for e in root.cssselect("a"):
-  print(e.get("href"))
+urls=[e.get("href") for e in root.cssselect("a")]
+while(len(urls)>0):
+  if url in urls[0]:
+    ur=urls[0]
+    print("scraping: "+ur)
+    html1= scraperwiki.scrape(urls[0])
+    root1 = lxml.html.fromstring(html1)
+    urls.pop()
+    newrls=[e.get("href") for e in root1.cssselect("a")]
+    urls=urls+newrls
+    try:
+      if root1.cssselect("div[class='blog-col']"):
+        scraperwiki.sqlite.save(unique_keys=[ur], data={"link": ur, "blog":root1.cssselect("div[class='blog-col']") })
+        print("got a blog!")
+      else:
+        print("no article for this link")
+        pass
+    except:pass
+  else:pass
+        
+    
+  
+    
   #scraperwiki.sqlite.save(unique_keys=[e.get("href")], data={"link": e.get("href")})
 
 #root.cssselect("div[class='blog-col']")
